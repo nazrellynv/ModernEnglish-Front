c48638fd65374f3d22aa9eb39d7382a9b7b67a50
nazrellyn <nazrinv07@gmail.com>
2023-12-23 17:33:46 +0300

    Responsive-2

--- src/js/owl.js
+++ src/js/owl.js
@@ -1,29 +1,52 @@
 $('#review-slider-1').owlCarousel({
-  loop:true,
-  margin:30,
-  nav:false,
-  dots: false,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:2
-      },
-  }
-})
-$('#review-slider-2').owlCarousel({
-  loop:true,
-  margin:30,
-  nav:false,
-  dots: false,
-  startPosition: 2,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:2
-      },
-  }
-})
+    loop:true,
+    margin:30,
+    nav:false,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+    }
+  })
+  $('#review-slider-2').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:false,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+          },
+          390:{
+              items: 1,
+              startPosition: 4,
+              center: true
+          },
+          600:{
+              startPosition: 2,
+              items:2
+        },
+    }
+  })
+  $('#mini-review-slider').owlCarousel({
+      loop:true,
+      margin:15,
+      nav:true,
+      center:true,
+      autoWidth:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:1
+          },
+          1000:{
+              items:5
+          }
+      }
+  })
